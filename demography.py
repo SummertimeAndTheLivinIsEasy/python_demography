1f153b66d8f937fa475bfbb5c57734d5d3f26e81
SummertimeAndTheLivinIsEasy <165676989+SummertimeAndTheLivinIsEasy@users.noreply.github.com>
2024-10-29 02:35:08 +0300

    add DB, css and html: base, index, content, faq, trip.

--- demography.py
+++ demography.py
@@ -35,30 +35,46 @@ Breadcrumbs(app=app)
 @register_breadcrumb(app, '.', 'Главная')
 def index():
     with Session() as session:
-        # tr_level = Trip_level(name='hghjh')
-        # session.add(tr_level)
         session.commit()
-    trip_level1 = session.get(Trip_level, 3)
-    trip_level2 = session.query(Trip_level).filter(Trip_level.name == 'hjh')
+    trip_level1 = session.get(Trip_type, 1)
+    trip_level2 = session.query(Trip_level).filter(Trip_level.level_name == 'Начинающий')
     return render_template('index.html', title='Главная', trip_level1=trip_level1, trip_level2=trip_level2)
 
 
-@app.route('/content')
-@register_breadcrumb(app, './content', 'Контент')
-def content():
+@app.route('/content/<type_name>')
+# @register_breadcrumb(app, './content/<type_name>', '<type_name>')
+def content(type_name: str):
     with Session() as session:
         session.commit()
-    trip_level2 = session.query(Trip_level).filter(Trip_level.name == 'hjh')
-    return render_template('content.html', title='Список походов', trip_level2=trip_level2)
+    # print(f"type_name: {type_name}")
+    type_id = session.query(Trip_type).filter(Trip_type.type_name == type_name).first()
+    trip_list = session.query(Trip, Trip_type, Trip_level, Trip_description, Trip_duration, Photo).join(Trip_type).join(Trip_level).join(Trip_description).join(Trip_duration).join(Photo).filter(Trip.trip_type_id == type_id.id)
+    # for (trip, type, level, description, duration, photo) in trip_list:
+    #     print(f"Document: {trip, type, level, description, duration}")
+    # trip_list = session.query(Trip).filter(Trip.trip_type_id == s1.id)
+    return render_template('content.html', title=type_name, trip_list=trip_list, type_id=type_id)
 
+@app.route('/trip/<int:trip_id>')
+# @register_breadcrumb(app, './content/<type_name>', '<type_name>')
+def trip(trip_id: int):
+    with Session() as session:
+        session.commit()
+    # print(f"type_name: {type_name}")
+    # trip = session.get(Trip, trip_id)
+    # type_id = session.query(Trip_type).filter(Trip_type.type_name == trip.trip_type_id).first()
+    trip_photos = session.query(Trip_photo, Photo).join(Photo).filter(Trip_photo.trip_id==trip_id).all()
+    len_trip_photos = len(trip_photos)
+    trip_description = session.query(Trip, Trip_type, Trip_level, Trip_description, Trip_duration, Photo).join(Trip_type).join(Trip_level).join(Trip_description).join(Trip_duration).join(Photo).filter(Trip.id == trip_id)
+    return render_template('trip.html', title=trip_description[0][1].type_name, trip_description=trip_description, trip_photos=trip_photos, len_trip_photos=len_trip_photos)
 
 @app.route('/faq')
-@register_breadcrumb(app, './faq', 'FAQ')
+# @register_breadcrumb(app, './faq', 'FAQ')
 def faq():
     return render_template('faq.html', title='FAQ')
 
 
 if __name__ == '__main__':
+
     app.run(debug=True)
 
     # engine = create_engine('sqlite:////tmp/test.db', echo=True)
--- models.py
+++ models.py
@@ -14,81 +14,120 @@ Base = declarative_base()
 
 # engine = create_engine("postgresql+psycopg2://postgres:1111@localhost/sqlalchemy_tuts")
 
+class Trip_duration(Base):
+    __tablename__ = 'trip_durations'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    duration = Column(Integer, nullable=False, unique=True)
+    trip = relationship("Trip", backref='trip_durations')
 
-# trip_photo = Table('trip_photo', Base.metadata,
-#     Column('trip_id', Integer(), ForeignKey("trips.id")),
-#     Column('photo_id', Integer(), ForeignKey("photos.id"))
-# )
-#
-# class Trip_type(Base):
-#     __tablename__ = 'trip_types'
-#     id = Column(Integer, primary_key=True, autoincrement=True)
-#     name = Column(String(100), nullable=False, unique=True)
-#     trip = relationship("Trip", backref='trip_types')
+class Trip_type(Base):
+    __tablename__ = 'trip_types'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    type_name = Column(String(100), nullable=False,  unique=True)
+    # # error https://sqlalche.me/e/20/gkpj
+    # id = Column(String(30), primary_key=True, nullable=False, unique=True)
+    trip = relationship("Trip", backref='trip_types')
 
 class Trip_level(Base):
     __tablename__ = 'trip_levels'
     id = Column(Integer, primary_key=True, autoincrement=True)
     # name = Column(String(100), nullable=False, unique=True)
-    name = Column(String(100), nullable=False)
-    # trip = relationship("Trip", backref='trip_levels')
+    level_name = Column(String(100), nullable=False, unique=True)
+    trip = relationship("Trip", backref='trip_levels')
 
     def __repr__(self):
         return f'{self.id}'
 
 
-# class Trip_description(Base):
-#     __tablename__ = 'trip_descriptions'
-#     id = Column(Integer, primary_key=True, autoincrement=True)
-#     title = Column(String(100), nullable=False, unique=True)
-#     how_to_go = Column(String, nullable=False)
-#     description = Column(String, nullable=False)
-#     track = Column(String, nullable=False)
-#     route_length = Column(Integer, nullable=False)
-#     official_website_link = Column(String, nullable=False)
-#     trip = relationship("Trip", backref='trip_descriptions')
-#
-# class Trip_photo(Base):
-#     __tablename__ = 'trip_photos'
-#     id = Column(Integer, primary_key=True, autoincrement=True)
-#     photo_id = Column(Integer, ForeignKey('photos.id'))
-#     trip_id = Column(Integer, ForeignKey('trips.id'))
-#
-# class Photo(Base):
-#     __tablename__ = 'photos'
-#     id = Column(Integer, primary_key=True, autoincrement=True)
-#     name = Column(String(100), nullable=False, unique=True)
-#     trips = relationship("Trip_photo", backref='photos')
-#
-#
-# class Trip(Base):
-#     __tablename__ = 'trips'
-#     id = Column(Integer, primary_key=True, autoincrement=True)
-#     trip_type_id = Column(Integer(), ForeignKey('trip_types.id'))
-#     trip_type = relationship("Trip_type")
-#     trip_level_id = Column(Integer(), ForeignKey('trip_levels.id'))
-#     trip_level = relationship("Trip_level", backref='trips')
-#     trip_description_id = Column(Integer(), ForeignKey('trip_descriptions.id'))
-#     trip_description = relationship("Trip_description")
-#     photos = relationship("Trip_photo", backref='trips')
-#     comments = relationship("Comment", backref='trips')
-#
-# class User(Base):
-#     __tablename__ = 'user'
-#     id = Column(Integer, primary_key=True, autoincrement=True)
-#     username = Column(String(100), nullable=False, unique=True)
-#     about_me = Column(String, nullable=True)
-#     email = Column(String(100), index=True,
-#                               unique=True, nullable=True)
-#     hashed_password = Column(String, nullable=True)
-#
-# class Comment(Base):
-#     __tablename__ = 'comment'
-#     id = Column(Integer, primary_key=True, autoincrement=True)
-#     description = Column(String, nullable=True)
-#     trip_id = Column(Integer, ForeignKey('trips.id'))
-#     trip = relationship("Trip")
+class Trip_description(Base):
+    __tablename__ = 'trip_descriptions'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    title = Column(String(100), nullable=False, unique=True)
+    how_to_go = Column(String, nullable=False)
+    description = Column(String, nullable=False)
+    track = Column(String, nullable=False)
+    route_length = Column(Integer, nullable=False)
+    official_website_link = Column(String, nullable=False)
+    trip = relationship("Trip", backref='trip_descriptions')
+    main_photo_id = Column(Integer(), ForeignKey('photos.id'))
+
+# trip_photo = Table('trip_photo', Base.metadata,
+#     Column('trip_id', Integer(), ForeignKey("trips.id")),
+#     Column('photo_id', Integer(), ForeignKey("photos.id"))
+# )
+
+class Photo(Base):
+    __tablename__ = 'photos'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    link = Column(String(100), nullable=False, unique=True)
+    photo_name = Column(String(100), nullable=False)
+    trips = relationship("Trip", secondary='trip_photos', back_populates='photos')
+    trip_description_id = relationship('Trip_description', backref='photos', uselist=False)
+
+class Trip(Base):
+    __tablename__ = 'trips'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    trip_type_id = Column(Integer(), ForeignKey('trip_types.id'))
+    trip_duration_id = Column(Integer(), ForeignKey('trip_durations.id'))
+    # # error https://sqlalche.me/e/20/gkpj
+    # trip_type_id = Column(String(30), ForeignKey('trip_types.id'))
+    # trip_type = relationship("Trip_type", backref='trips')
+    trip_level_id = Column(Integer(), ForeignKey('trip_levels.id'))
+    # trip_level = relationship("Trip_level", backref='trips')
+    trip_description_id = Column(Integer(), ForeignKey('trip_descriptions.id'))
+    # trip_description = relationship("Trip_description", backref='trips')
+    photos = relationship("Photo", secondary='trip_photos', back_populates='trips')
+    comments = relationship("Comment", backref='trips')
+
+class Trip_photo(Base):
+    __tablename__ = 'trip_photos'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    trip_id = Column(Integer, ForeignKey('trips.id'))
+    photo_id = Column(Integer, ForeignKey('photos.id'))
+
+class User(Base):
+    __tablename__ = 'user'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    username = Column(String(100), nullable=False, unique=True)
+    about_me = Column(String, nullable=True)
+    email = Column(String(100), index=True,
+                              unique=True, nullable=True)
+    hashed_password = Column(String, nullable=True)
+    comment = relationship("Comment", backref="user")
+
+class Comment(Base):
+    __tablename__ = 'comment'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    description = Column(String, nullable=True)
+    trip_id = Column(Integer, ForeignKey('trips.id'))
+    # trip = relationship("Trip", backref='comments')
+    user_id = Column(Integer(), ForeignKey('user.id'))
 #
 
 
-Base.metadata.create_all(engine)
+# Base.metadata.create_all(engine)
+
+
+
+    # parent = Parent(name='John Doe', uuid=str(uuid.uuid4()))
+    # session.add(parent)
+    # session.commit()
+    #
+    # child = Child(name='Jimmy Doe', uuid=str(uuid.uuid4()))
+    # session.add(child)
+    # session.commit()
+    #
+    # parent.children.append(child)
+    #
+    # print('# Parents')
+    # for parent in session.query(Parent).all():
+    #     children = [x.name for x in parent.children]
+    #     print(f'Parent: name={parent.name}, children={children}')
+    # print()
+    #
+    # print('# Children')
+    # for child in session.query(Child).all():
+    #     parents = [x.name for x in child.parents]
+    #     print(f'Child: name={child.name}, parents={parents}')
+    # print()
+
