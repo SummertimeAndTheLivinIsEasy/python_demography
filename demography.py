5eacc81f98e63a73831380a5594895e95d406bc2
SummertimeAndTheLivinIsEasy <165676989+SummertimeAndTheLivinIsEasy@users.noreply.github.com>
2024-11-14 02:52:38 +0300

    add FilterForm

--- demography.py
+++ demography.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask import render_template, flash, redirect, url_for
+from flask import render_template, flash, redirect, url_for, request
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 # для хлебных крошек начало
@@ -9,7 +9,7 @@ from flask_menu import Menu
 import models
 # from models import Trip, Trip_type, Trip_level, Trip_description, Trip_duration, Photo, Trip_photo, Session, User
 from sqlalchemy import select
-from forms import LoginForm, RegistrationForm, CommentForm
+from forms import LoginForm, RegistrationForm, CommentForm, FilterForm
 from config import Config
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import LoginManager, current_user, login_user, logout_user
@@ -91,19 +91,48 @@ def index():
     trip_level2 = session.query(models.Trip_level).filter(models.Trip_level.level_name == 'Начинающий')
     return render_template('index.html', title='Главная', trip_level1=trip_level1, trip_level2=trip_level2)
 
-
-@app.route('/content/<type_name>')
+@app.route('/content/<type_name>/', defaults={'duration': "---", 'level': "---"}, methods=['GET', 'POST'])
+@app.route('/content/<type_name>/<duration>/', defaults={'level': "---"}, methods=['GET', 'POST'])
+@app.route('/content/<type_name>/<duration>/<level>', methods=['GET', 'POST'])
+# @app.route('/content/<type_name>')
 # @register_breadcrumb(app, './content/<type_name>', '<type_name>')
-def content(type_name: str):
+def content(type_name: str, duration: str, level: str):
+# def content(type_name: str):
+#     form = FilterForm()
+
+    if duration == "---":
+        form = FilterForm(duration=duration, level=level)
+    else:
+        form = FilterForm(duration=f"{duration[0]} день" if duration[0] == "1" else f"{duration[0]} дня", level=level)
+    # form = FilterForm()
     with models.Session() as session:
         session.commit()
     # print(f"type_name: {type_name}")
     type_id = session.query(models.Trip_type).filter(models.Trip_type.type_name == type_name).first()
     trip_list = session.query(models.Trip, models.Trip_type, models.Trip_level, models.Trip_description, models.Trip_duration, models.Photo).join(models.Trip_type).join(models.Trip_level).join(models.Trip_description).join(models.Trip_duration).join(models.Photo).filter(models.Trip.trip_type_id == type_id.id)
+    if duration != "---":
+        if level == "---":
+            trip_list = trip_list.filter(models.Trip_duration.id == int(duration[0])).all()
+        else:
+            trip_list = trip_list.filter(models.Trip_duration.id == int(duration[0])).filter(
+                models.Trip_level.level_name == level).all()
+
+    else:
+        if level != "---":
+            trip_list = trip_list.filter(models.Trip_level.level_name == level).all()
+
+
+    if form.validate_on_submit():
+        if form.show_btn:
+            return redirect(url_for('content', title=type_name, type_name=type_name, duration=form.duration.data, level=form.level.data, type_id=type_id, form=form))
+
+
+
     # for (trip, type, level, description, duration, photo) in trip_list:
     #     print(f"Document: {trip, type, level, description, duration}")
     # trip_list = session.query(Trip).filter(Trip.trip_type_id == s1.id)
-    return render_template('content.html', title=type_name, trip_list=trip_list, type_id=type_id)
+    # len_trip_list
+    return render_template('content.html', title=type_name, trip_list=trip_list, type_id=type_id, form=form)
 
 @app.route('/trip/<int:trip_id>', methods=['GET', 'POST'])
 # @register_breadcrumb(app, './content/<type_name>', '<type_name>')
@@ -119,6 +148,11 @@ def trip(trip_id: int):
             else:
                 flash('Комментарии могут оставлять только зарегистрированные пользователи')
             return redirect(url_for('trip', trip_id=trip_id))
+        if form.update_btn:
+            l = request.form["action"]
+            print(f"request.form['action']: {l}")
+            # print(f"select from update_btn: {str(select)}") # just to see what select is
+            return redirect(url_for('trip', trip_id=trip_id))
 
 
 
@@ -131,8 +165,6 @@ def trip(trip_id: int):
     len_trip_photos = len(trip_photos)
     trip_description = session.query(models.Trip, models.Trip_type, models.Trip_level, models.Trip_description, models.Trip_duration, models.Photo).join(models.Trip_type).join(models.Trip_level).join(models.Trip_description).join(models.Trip_duration).join(models.Photo).filter(models.Trip.id == trip_id)
     trip_comments = session.query(models.Comment, models.User).join(models.User).filter(models.Comment.trip_id == trip_id).order_by(models.Comment.id.desc())
-    # for i in trip_comments:
-    #     print(f"i для trip_comments: {i[0]}, {i}")
     return render_template('trip.html', title=trip_description[0][1].type_name, trip_description=trip_description, trip_photos=trip_photos, len_trip_photos=len_trip_photos, form=form, trip_comments=trip_comments)
 
 @app.route('/faq')
--- forms.py
+++ forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField, SelectField
 from wtforms.validators import DataRequired, Email, EqualTo, ValidationError, Length
 import models
 
@@ -53,7 +53,30 @@ class CommentForm(FlaskForm):
 
 
   comment = TextAreaField('Say something', validators=[DataRequired(), Length(min=10, max=1000)], render_kw={'style': 'width: 100%', "placeholder": "Введите комментарий"})
-  add_btn = SubmitField('Добавить', render_kw={'style': 'width: 20%'})
+  add_btn = SubmitField('Добавить', render_kw={'style': 'width: 25%'})
   update_btn = SubmitField('Изменить', render_kw={'style': 'width: 20%'})
   delete_btn = SubmitField('Удалить', render_kw={'style': 'width: 20%'})
+
+
+class FilterForm(FlaskForm):
+  class Meta:
+    locales = ['ru-RU', 'ru']
+
+    def get_translations(self, form):
+      return super(FlaskForm.Meta, self).get_translations(form)
+
+  with models.Session() as session:
+    durations = session.query(models.Trip_duration).all()
+    durations_list = ["---"] + [f"{i.duration} день" if i.id == 1 else f"{i.duration} дня" for i in durations]
+
+    levels = session.query(models.Trip_level).all()
+    levels_list = ["---"] + [i.level_name for i in levels]
+
+
+  duration = SelectField(u'Длительность', choices=durations_list)
+  level = SelectField('Уровень подготовки', choices=levels_list)
+
+  show_btn = SubmitField('Показать')
+
+
 
